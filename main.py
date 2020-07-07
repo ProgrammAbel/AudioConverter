830cd3c7777f796fb4b38e1b1c9eb1427f33f2db
Abel Binoop <abelbinoop@live.co.uk>
2020-07-07 09:18:23 +0100

    feat: add conversion functionality

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 import tkinter as tk
+import ffmpeg
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+filepath = ""
+
+def open_file():
+    global filepath
+    filepath = askopenfilename(
+                      filetypes=[("Video Files", "*.mp4")]
+                      )
+
+    if not filepath:
+        return
+
+def convert_file():
+    converted_filepath = asksaveasfilename(
+            defaultextension=".mp3",
+            filetypes=[("Audio Files", "*.mp3")]
+    )
+
+    ffmpeg.input(filepath).output(converted_filepath).run()
 
 window = tk.Tk()
 window.title("AudioConverter")
@@ -7,9 +28,9 @@ window.rowconfigure(0, minsize=150, weight=1)
 window.columnconfigure(1, minsize=300, weight=1)
 
 fr_buttons = tk.Frame(window)
-btn_open = tk.Button(fr_buttons, text="Open")
+btn_open = tk.Button(fr_buttons, text="Open", command=open_file)
 label_open = tk.Label(fr_buttons, text="No file opened...")
-btn_save = tk.Button(fr_buttons, text="Save As...")
+btn_save = tk.Button(fr_buttons, text="Save As...", command=convert_file)
 label_save = tk.Label(fr_buttons, text="No target location...")
 
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
